9e76c2a4f14d04de61374b17f862dfbcd15b0b92
ReejaBasukala <basukalareeja@gmail.com>
2019-09-09 09:07:53 +0545

    week2

---
+++ week1/hello_toast/app/src/main/java/com/example/hello_toast/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.hello_toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private TextView textView;
+    int count = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView =findViewById(R.id.textView);
+    }
+
+    public void toastClick(View view){
+        Toast.makeText(this, "Hello Toast!", Toast.LENGTH_LONG).show();
+    }
+
+    public void Toast(View view){
+        count++;
+        textView.setText(""+count);
+    }
+}
